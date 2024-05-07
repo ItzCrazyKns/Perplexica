68837e06ee2eb2d2e778f5dd036e0df6aa7ee56b
ItzCrazyKns <kushagra20103@gmail.com>
2024-05-07 11:52:53 +0530

    feat(embedding-providers): add local models

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -1,6 +1,7 @@
 import { ChatOpenAI, OpenAIEmbeddings } from '@langchain/openai';
 import { ChatOllama } from '@langchain/community/chat_models/ollama';
 import { OllamaEmbeddings } from '@langchain/community/embeddings/ollama';
+import { HuggingFaceTransformersEmbeddings } from './huggingfaceTransformer';
 import {
   getGroqApiKey,
   getOllamaApiEndpoint,
@@ -156,6 +157,12 @@ export const getAvailableEmbeddingModelProviders = async () => {
         });
         return acc;
       }, {});
+
+      models['local'] = {
+        'GTE Small': new HuggingFaceTransformersEmbeddings({
+          modelName: 'Xenova/gte-small',
+        }),
+      };
     } catch (err) {
       logger.error(`Error loading Ollama embeddings: ${err}`);
     }
