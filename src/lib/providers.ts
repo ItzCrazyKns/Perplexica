321e60b9931d7e497e0698ba3564b52c3d6fa855
ItzCrazyKns <kushagra20103@gmail.com>
2024-05-07 12:33:44 +0530

    feat(embedding-providers): load separately, add bert & bge

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -158,14 +158,25 @@ export const getAvailableEmbeddingModelProviders = async () => {
         return acc;
       }, {});
 
-      models['local'] = {
-        'GTE Small': new HuggingFaceTransformersEmbeddings({
-          modelName: 'Xenova/gte-small',
-        }),
-      };
     } catch (err) {
       logger.error(`Error loading Ollama embeddings: ${err}`);
     }
+  }
+
+  try {
+    models['local'] = {
+      'BGE Small': new HuggingFaceTransformersEmbeddings({
+        modelName: 'Xenova/bge-small-en-v1.5',
+      }),
+      'GTE Small': new HuggingFaceTransformersEmbeddings({
+        modelName: 'Xenova/gte-small',
+      }),
+      'Bert Multilingual': new HuggingFaceTransformersEmbeddings({
+        modelName: 'Xenova/bert-base-multilingual-uncased'
+      }),
+    };
+  } catch(err) {
+    logger.error(`Error loading local embeddings: ${err}`); 
   }
 
   return models;
