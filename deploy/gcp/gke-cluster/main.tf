27d7b000d0701289120cb40795db1426ff59ea87
Hristo <53634432+izo0x90@users.noreply.github.com>
2024-05-21 14:31:14 -0400

    Add AI/ML infrence scope to OAuth credentials requested for cluster IAM account

--- deploy/gcp/gke-cluster/main.tf
+++ deploy/gcp/gke-cluster/main.tf
@@ -51,6 +51,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     disk_size_gb = 25
     spot = true
     oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
