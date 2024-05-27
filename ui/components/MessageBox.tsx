d28cfa3319fca349b83c0887d6df487d521e46b1
WanQuanXie <i2cherry941219@gmail.com>
2024-05-28 07:47:45 +0800

    fix(MessageBox): <code/> type message text-color adapt light mode

--- ui/components/MessageBox.tsx
+++ ui/components/MessageBox.tsx
@@ -106,7 +106,7 @@ const MessageBox = ({
                   Answer
                 </h3>
               </div>
-              <Markdown className="prose prose-strong:text-black dark:prose-strong:text-white dark:prose-invert max-w-none break-words prose-invert prose-p:leading-relaxed prose-pre:p-0 text-black dark:text-white text-sm md:text-base font-medium">
+              <Markdown className="prose dark:prose-invert prose-strong:text-black dark:prose-strong:text-white prose-code:text-black dark:prose-code:text-white max-w-none break-words prose-invert prose-p:leading-relaxed prose-pre:p-0 text-black dark:text-white text-sm md:text-base font-medium">
                 {parsedMessage}
               </Markdown>
               {loading && isLast ? null : (
