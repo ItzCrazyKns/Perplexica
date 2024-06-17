3e50ae9c13891bf2b0c0f75fe2cf981510a913bb
Joseph Lee <cgjosephlee@gmail.com>
2024-06-17 16:18:41 +0800

    Fix a trivial typo

--- src/agents/webSearchAgent.ts
+++ src/agents/webSearchAgent.ts
@@ -44,7 +44,7 @@ Rephrased question:
 const basicWebSearchResponsePrompt = `
     You are Perplexica, an AI model who is expert at searching the web and answering user's queries.
 
-    Generate a response that is informative and relevant to the user's query based on provided context (the context consits of search results containg a brief description of the content of that page).
+    Generate a response that is informative and relevant to the user's query based on provided context (the context consits of search results containing a brief description of the content of that page).
     You must use this context to answer the user's query in the best way possible. Use an unbaised and journalistic tone in your response. Do not repeat the text.
     You must not tell the user to open any link or visit any website to get the answer. You must provide the answer in the response itself. If the user asks for links you can provide them.
     Your responses should be medium to long in length be informative and relevant to the user's query. You can use markdowns to format your response. You should use bullet points to list the information. Make sure the answer is not short and is informative.
