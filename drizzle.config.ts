93b90dc1c4f7a38f0540c45531349ca2998b31b6
ItzCrazyKns <kushagra20103@gmail.com>
2024-06-29 11:08:11 +0530

    feat(db): create schema & config files

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  dialect: 'sqlite',
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dbCredentials: {
+    url: './data/db.sqlite',
+  },
+});
---
+++ src/db/schema.ts
@@ -0,0 +1,19 @@
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const messages = sqliteTable('messages', {
+  id: integer('id').primaryKey(),
+  content: text('content').notNull(),
+  chatId: text('chatId').notNull(),
+  messageId: text('messageId').notNull(),
+  role: text('type', { enum: ['assistant', 'user'] }),
+  metadata: text('metadata', {
+    mode: 'json',
+  }),
+});
+
+export const chats = sqliteTable('chats', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  createdAt: text('createdAt').notNull(),
+  focusMode: text('focusMode').notNull(),
+});
