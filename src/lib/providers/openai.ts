9195cbcce0a1e5058d95e22e8bc79cdf74cd82bf
ItzCrazyKns <kushagra20103@gmail.com>
2024-07-20 09:26:46 +0530

    feat(openai): add GPT-4 Omni mini

--- src/lib/providers/openai.ts
+++ src/lib/providers/openai.ts
@@ -29,6 +29,11 @@ export const loadOpenAIChatModels = async () => {
         modelName: 'gpt-4o',
         temperature: 0.7,
       }),
+      'GPT-4 omni mini': new ChatOpenAI({
+        openAIApiKey,
+        modelName: 'gpt-4o-mini',
+        temperature: 0.7,
+      }),
     };
 
     return chatModels;
