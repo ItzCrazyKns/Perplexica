27f8cfd212ac1f0b3ee18a6b9737b7a7636bb03f
ItzCrazyKns <kushagra20103@gmail.com>
2024-07-25 20:33:56 +0530

    feat(toast): fix theme colors

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
               unstyled: true,
               classNames: {
                 toast:
-                  'bg-light-primary dark:bg-dark-primary text-white rounded-lg p-4 flex flex-row items-center space-x-2',
+                  'bg-light-primary dark:bg-dark-secondary dark:text-white/70 text-black-70 rounded-lg p-4 flex flex-row items-center space-x-2',
               },
             }}
           />
