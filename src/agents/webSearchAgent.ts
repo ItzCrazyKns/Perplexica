8e4f0c6a6d247fc075431346d09e7ad5e784bdb1
ItzCrazyKns <kushagra20103@gmail.com>
2024-07-30 10:09:05 +0530

    feat(web-search): add URL & PDF searching capibilities

--- src/agents/webSearchAgent.ts
+++ src/agents/webSearchAgent.ts
@@ -19,20 +19,47 @@ import formatChatHistoryAsString from '../utils/formatHistory';
 import eventEmitter from 'events';
 import computeSimilarity from '../utils/computeSimilarity';
 import logger from '../utils/logger';
+import LineListOutputParser from '../lib/outputParsers/listLineOutputParser';
+import { getDocumentsFromLinks } from '../lib/linkDocument';
+import LineOutputParser from '../lib/outputParsers/lineOutputParser';
 
 const basicSearchRetrieverPrompt = `
 You will be given a conversation below and a follow up question. You need to rephrase the follow-up question if needed so it is a standalone question that can be used by the LLM to search the web for information.
 If it is a writing task or a simple hi, hello rather than a question, you need to return \`not_needed\` as the response.
+If the question contains some links and asks to answer from those links or even if they don't you need to return the links inside 'links' XML block and the question inside 'question' XML block. If there are no links then you need to return the question without any XML block.
+If the user asks to summarrize the content from some links you need to return \`Summarize\` as the question inside the 'question' XML block and the links inside the 'links' XML block.
 
 Example:
 1. Follow up question: What is the capital of France?
-Rephrased: Capital of france
+Rephrased question: \`Capital of france\`
 
 2. Follow up question: What is the population of New York City?
-Rephrased: Population of New York City
+Rephrased question: \`Population of New York City\`
 
 3. Follow up question: What is Docker?
-Rephrased: What is Docker
+Rephrased question: \`What is Docker\`
+
+4. Follow up question: Can you tell me what is X from https://example.com
+Rephrased question: \`
+<question>
+Can you tell me what is X?
+</question>
+
+<links>
+https://example.com
+</links>
+\`
+
+5. Follow up question: Summarize the content from https://example.com
+Rephrased question: \`
+<question>
+Summarize
+</question>
+
+<links>
+https://example.com
+</links>
+\`
 
 Conversation:
 {chat_history}
@@ -42,11 +69,13 @@ Rephrased question:
 `;
 
 const basicWebSearchResponsePrompt = `
-    You are Perplexica, an AI model who is expert at searching the web and answering user's queries.
+    You are Perplexica, an AI model who is expert at searching the web and answering user's queries. You are also an expert at summarizing web pages or documents and searching for content in them.
 
     Generate a response that is informative and relevant to the user's query based on provided context (the context consits of search results containing a brief description of the content of that page).
     You must use this context to answer the user's query in the best way possible. Use an unbaised and journalistic tone in your response. Do not repeat the text.
     You must not tell the user to open any link or visit any website to get the answer. You must provide the answer in the response itself. If the user asks for links you can provide them.
+    If the query contains some links and the user asks to answer from those links you will be provided the entire content of the page inside the \`context\` XML block. You can then use this content to answer the user's query.
+    If the user asks to summarize content from some links, you will be provided the entire content of the page inside the \`context\` XML block. You can then use this content to summarize the text. The content provided inside the \`context\` block will be already summarized by another model so you just need to use that content to answer the user's query.
     Your responses should be medium to long in length be informative and relevant to the user's query. You can use markdowns to format your response. You should use bullet points to list the information. Make sure the answer is not short and is informative.
     You have to cite the answer using [number] notation. You must cite the sentences with their relevent context number. You must cite each and every part of the answer so the user can know where the information is coming from.
     Place these citations at the end of that particular sentence. You can cite the same sentence multiple times if it is relevant to the user's query like [number1][number2].
@@ -59,7 +88,7 @@ const basicWebSearchResponsePrompt = `
     {context}
     </context>
 
-    If you think there's nothing relevant in the search results, you can say that 'Hmm, sorry I could not find any relevant information on this topic. Would you like me to search again or ask something else?'.
+    If you think there's nothing relevant in the search results, you can say that 'Hmm, sorry I could not find any relevant information on this topic. Would you like me to search again or ask something else?'. You do not need to do this for summarization tasks.
     Anything between the \`context\` is retrieved from a search engine and is not a part of the conversation with the user. Today's date is ${new Date().toISOString()}
 `;
 
@@ -112,23 +141,100 @@ const createBasicWebSearchRetrieverChain = (llm: BaseChatModel) => {
         return { query: '', docs: [] };
       }
 
-      const res = await searchSearxng(input, {
-        language: 'en',
+      const linksOutputParser = new LineListOutputParser({
+        key: 'links',
       });
 
-      const documents = res.results.map(
-        (result) =>
-          new Document({
-            pageContent: result.content,
-            metadata: {
-              title: result.title,
-              url: result.url,
-              ...(result.img_src && { img_src: result.img_src }),
-            },
-          }),
-      );
+      const questionOutputParser = new LineOutputParser({
+        key: 'question',
+      });
 
-      return { query: input, docs: documents };
+      const links = await linksOutputParser.parse(input);
+      let question = await questionOutputParser.parse(input);
+
+      if (links.length > 0) {
+        if (question.length === 0) {
+          question = 'Summarize';
+        }
+
+        let docs = []
+
+        const linkDocs = await getDocumentsFromLinks({ links });
+
+        const docGroups: Document[] = [];
+
+        linkDocs.map((doc) => {
+          const URLDocExists = docGroups.find((d) => d.metadata.url === doc.metadata.url && d.metadata.totalDocs < 10);
+
+          if (!URLDocExists) {
+            docGroups.push({
+              ...doc,
+              metadata: {
+                ...doc.metadata,
+                totalDocs: 1
+              }
+            });
+          }
+
+          const docIndex = docGroups.findIndex((d) => d.metadata.url === doc.metadata.url && d.metadata.totalDocs < 10);
+
+          if (docIndex !== -1) {
+            docGroups[docIndex].pageContent = docGroups[docIndex].pageContent + `\n\n` +  doc.pageContent;
+            docGroups[docIndex].metadata.totalDocs += 1;
+          }
+        })
+
+        await Promise.all(docGroups.map(async (doc) => {
+          const res = await llm.invoke(`
+            You are a text summarizer. You need to summarize the text provided inside the \`text\` XML block. 
+            You need to summarize the text into 1 or 2 sentences capturing the main idea of the text.
+            You need to make sure that you don't miss any point while summarizing the text.
+            You will also be given a \`query\` XML block which will contain the query of the user. Try to answer the query in the summary from the text provided.
+            If the query says Summarize then you just need to summarize the text without answering the query.
+            Only return the summarized text without any other messages, text or XML block.
+
+            <query>
+            ${question}
+            </query>
+
+            <text>
+            ${doc.pageContent}
+            </text>
+
+            Make sure to answer the query in the summary.
+          `);
+
+          const document = new Document({
+            pageContent: res.content as string,
+            metadata: {
+              title: doc.metadata.title,
+              url: doc.metadata.url,
+            },
+          })
+          
+          docs.push(document)
+        }))
+
+        return { query: question, docs: docs };
+      } else {
+        const res = await searchSearxng(input, {
+          language: 'en',
+        });
+
+        const documents = res.results.map(
+          (result) =>
+            new Document({
+              pageContent: result.content,
+              metadata: {
+                title: result.title,
+                url: result.url,
+                ...(result.img_src && { img_src: result.img_src }),
+              },
+            }),
+        );
+
+        return { query: input, docs: documents };
+      }
     }),
   ]);
 };
@@ -153,6 +259,10 @@ const createBasicWebSearchAnsweringChain = (
     docs: Document[];
   }) => {
     if (docs.length === 0) {
+      return docs;
+    }
+
+    if (query === 'Summarize') {
       return docs;
     }
 
