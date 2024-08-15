f11180db7ad871f306964bb98ffbc4f1c0ab356a
Andrew Pennington <email@andrew-pennington.dev>
2024-08-15 23:33:01 +0100

    Restructure

--- ui/pages/api/env.ts
+++ ui/pages/api/env.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from 'next/server';
+
 import process from 'process';
 
-export async function GET() {
-  return NextResponse.json({
+export default function handler(req, res) {
+  const envVars = {
     BACKEND_API_URL: process.env.BACKEND_API_URL,
     BACKEND_WS_URL: process.env.BACKEND_WS_URL
-  });
+  }
+  res.status(200).json(envVars);
 }
