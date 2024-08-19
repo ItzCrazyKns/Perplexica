622851c2ed16ce1ca6eb90ab700f471739c4ecca
Andrew Pennington <email@andrew-pennington.dev>
2024-08-19 12:17:02 +0100

    Remove return definition

--- ui/app/api/env/route.ts
+++ ui/app/api/env/route.ts
@@ -1,7 +1,7 @@
 import process from 'process';
 import { NextResponse } from 'next/server';
 
-export async function GET(_request: Request): NextResponse {
+export async function GET(_request: Request) {
   // Access environment variables
   const envVars = {
     'BACKEND_API_URL': process.env.BACKEND_API_URL,
