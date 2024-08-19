14b6924b02c16d8ef059a756300b95df19186f75
Andrew Pennington <email@andrew-pennington.dev>
2024-08-19 12:14:24 +0100

    Rework env api route

--- ui/app/api/env/route.ts
+++ ui/app/api/env/route.ts
@@ -1,15 +1,13 @@
 import process from 'process';
 import { NextResponse } from 'next/server';
 
-async function requestHandler(_request: Request): NextResponse {
+export async function GET(_request: Request): NextResponse {
   // Access environment variables
   const envVars = {
-    BACKEND_API_URL: process.env.BACKEND_API_URL,
-    BACKEND_WS_URL: process.env.BACKEND_WS_URL
+    'BACKEND_API_URL': process.env.BACKEND_API_URL,
+    'BACKEND_WS_URL': process.env.BACKEND_WS_URL
   }
 
   // Return the environment variables as a JSON response
   return NextResponse.json(envVars);
 }
-
-export { requestHandler as GET };
