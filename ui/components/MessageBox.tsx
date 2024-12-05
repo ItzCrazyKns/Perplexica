db7407bfac20825333da330aaf9869be06722910
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2024-12-05 20:19:41 +0530

    feat(messageBox): style markdown

--- ui/components/MessageBox.tsx
+++ ui/components/MessageBox.tsx
@@ -107,8 +107,8 @@ const MessageBox = ({
               </div>
               <Markdown
                 className={cn(
-                  'prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0',
-                  'max-w-none break-words text-black dark:text-white text-sm md:text-base font-medium',
+                  'prose prose-h1:mb-3 prose-h2:mb-2 prose-h2:mt-6 prose-h2:font-[800] prose-h3:mt-4 prose-h3:mb-1.5 prose-h3:font-[600] dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 font-[400]',
+                  'max-w-none break-words text-black dark:text-white',
                 )}
               >
                 {parsedMessage}
