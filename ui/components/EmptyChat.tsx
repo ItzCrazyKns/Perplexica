15f64a0ef070e1f5e924116a4ca84b6f983fb514
litongjava <litongjava@qq.com>
2025-02-03 14:35:02 -1000

    feat: move up of empty chat

--- ui/components/EmptyChat.tsx
+++ ui/components/EmptyChat.tsx
@@ -40,8 +40,8 @@ const EmptyChat = ({
           onClick={() => setIsSettingsOpen(true)}
         />
       </div>
-      <div className="flex flex-col items-center justify-center min-h-screen max-w-screen-sm mx-auto p-2 space-y-8">
-        <h2 className="text-black/70 dark:text-white/70 text-3xl font-medium -mt-8">
+      <div className="flex flex-col items-center max-w-screen-sm mx-auto p-2 pt-16 mt-16 space-y-8">
+        <h2 className="text-black/70 dark:text-white/70 text-5xl font-medium -mt-8">
           Research begins here.
         </h2>
         <EmptyChatMessageInput
