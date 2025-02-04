c2f4fb1dc942994769dc9ebac9f97107bc202325
litongjava <litongjava@qq.com>
2025-02-03 14:45:45 -1000

    feat: show deepseek

--- ui/components/MessageInputActions/Focus.tsx
+++ ui/components/MessageInputActions/Focus.tsx
@@ -133,7 +133,7 @@ const Focus = ({
         leaveTo="opacity-0 translate-y-1"
       >
         <PopoverPanel className="absolute z-10 w-64 md:w-[500px] left-0">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-light-primary dark:bg-dark-primary border rounded-lg border-light-200 dark:border-dark-200 w-full p-4 max-h-[200px] md:max-h-none overflow-y-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 bg-light-primary dark:bg-dark-primary border rounded-lg border-light-200 dark:border-dark-200 w-full p-4 max-h-[calc(100vh-6rem)] md:max-h-none overflow-y-auto pb-20">
             {focusModes.map((mode, i) => (
               <PopoverButton
                 onClick={() => setFocusMode(mode.key)}
