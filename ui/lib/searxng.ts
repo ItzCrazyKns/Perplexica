5c313e9bed47b7ee7fe7576780b1c71ac0507f44
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-03-18 10:23:59 +0530

    feat(ui): update packages, add config, add searxng

---
+++ ui/lib/searxng.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import { getSearxngApiEndpoint } from './config';
+
+interface SearxngSearchOptions {
+  categories?: string[];
+  engines?: string[];
+  language?: string;
+  pageno?: number;
+}
+
+interface SearxngSearchResult {
+  title: string;
+  url: string;
+  img_src?: string;
+  thumbnail_src?: string;
+  thumbnail?: string;
+  content?: string;
+  author?: string;
+  iframe_src?: string;
+}
+
+export const searchSearxng = async (
+  query: string,
+  opts?: SearxngSearchOptions,
+) => {
+  const searxngURL = getSearxngApiEndpoint();
+
+  const url = new URL(`${searxngURL}/search?format=json`);
+  url.searchParams.append('q', query);
+
+  if (opts) {
+    Object.keys(opts).forEach((key) => {
+      const value = opts[key as keyof SearxngSearchOptions];
+      if (Array.isArray(value)) {
+        url.searchParams.append(key, value.join(','));
+        return;
+      }
+      url.searchParams.append(key, value as string);
+    });
+  }
+
+  const res = await axios.get(url.toString());
+
+  const results: SearxngSearchResult[] = res.data.results;
+  const suggestions: string[] = res.data.suggestions;
+
+  return { results, suggestions };
+};
--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       },
     ],
   },
+  serverExternalPackages: ['pdf-parse']
 };
 
 export default nextConfig;
