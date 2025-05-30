740ff941a57351d6200700b3e45aee112e720a3a
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-05-30 08:35:15 +0530

    feat(discover): add preview mode

--- src/app/api/discover/route.ts
+++ src/app/api/discover/route.ts
@@ -16,28 +16,43 @@ const topics = ['AI', 'tech']; /* TODO: Add UI to customize this */
 
 export const GET = async (req: Request) => {
   try {
-    const data = (
-      await Promise.all([
-        ...new Array(articleWebsites.length * topics.length)
-          .fill(0)
-          .map(async (_, i) => {
-            return (
-              await searchSearxng(
-                `site:${articleWebsites[i % articleWebsites.length]} ${
-                  topics[i % topics.length]
-                }`,
-                {
-                  engines: ['bing news'],
-                  pageno: 1,
-                },
-              )
-            ).results;
-          }),
-      ])
-    )
-      .map((result) => result)
-      .flat()
-      .sort(() => Math.random() - 0.5);
+    const params = new URL(req.url).searchParams;
+    const mode: 'normal' | 'preview' =
+      (params.get('mode') as 'normal' | 'preview') || 'normal';
+
+    let data = [];
+
+    if (mode === 'normal') {
+      data = (
+        await Promise.all([
+          ...new Array(articleWebsites.length * topics.length)
+            .fill(0)
+            .map(async (_, i) => {
+              return (
+                await searchSearxng(
+                  `site:${articleWebsites[i % articleWebsites.length]} ${
+                    topics[i % topics.length]
+                  }`,
+                  {
+                    engines: ['bing news'],
+                    pageno: 1,
+                  },
+                )
+              ).results;
+            }),
+        ])
+      )
+        .map((result) => result)
+        .flat()
+        .sort(() => Math.random() - 0.5);
+    } else {
+      data = (
+        await searchSearxng(
+          `site:${articleWebsites[Math.floor(Math.random() * articleWebsites.length)]} ${topics[Math.floor(Math.random() * topics.length)]}`,
+          { engines: ['bing news'], pageno: 1 },
+        )
+      ).results;
+    }
 
     return Response.json(
       {
