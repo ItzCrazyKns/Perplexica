722f4f760e33eb66830626bdff1d6797da397777
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-07-15 21:45:37 +0530

    feat(manifest): update icons & screenshots

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,16 +10,44 @@ export default function manifest(): MetadataRoute.Manifest {
     display: 'standalone',
     background_color: '#0a0a0a',
     theme_color: '#0a0a0a',
+    screenshots: [
+      {
+        src: '/screenshots/p1.png',
+        form_factor: 'wide',
+        sizes: '2560x1600',
+      },
+      {
+        src: '/screenshots/p2.png',
+        form_factor: 'wide',
+        sizes: '2560x1600',
+      },
+      {
+        src: '/screenshots/p1_small.png',
+        form_factor: 'narrow',
+        sizes: '828x1792',
+      },
+      {
+        src: '/screenshots/p2_small.png',
+        form_factor: 'narrow',
+        sizes: '828x1792',
+      },
+    ],
     icons: [
       {
-        src: '/icons8-search-50.png',
+        src: '/icon-50.png',
         sizes: '50x50',
         type: 'image/png' as const,
       },
       {
-        src: '/icons8-search-100.png',
+        src: '/icon-100.png',
         sizes: '100x100',
         type: 'image/png',
+      },
+      {
+        src: '/icon.png',
+        sizes: '440x440',
+        type: 'image/png',
+        purpose: 'any',
       },
     ],
   };
