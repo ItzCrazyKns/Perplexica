0c7566bb8701f43f8d303cd8f672dba42cb30692
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-10-16 18:03:40 +0530

    feat(sidebar): fix colors on smaller devices

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -70,7 +70,7 @@ const Sidebar = ({ children }: { children: React.ReactNode }) => {
         </div>
       </div>
 
-      <div className="fixed bottom-0 w-full z-50 flex flex-row items-center gap-x-6 bg-light-primary dark:bg-dark-primary px-4 py-4 shadow-sm lg:hidden">
+      <div className="fixed bottom-0 w-full z-50 flex flex-row items-center gap-x-6 bg-light-secondary dark:bg-dark-secondary px-4 py-4 shadow-sm lg:hidden">
         {navLinks.map((link, i) => (
           <Link
             href={link.href}
