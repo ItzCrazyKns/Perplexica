8a1052e82b8b1e7c9eda549177cc779c38978b4b
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-10-18 15:08:06 +0530

    feat(providers): add ollama

--- src/lib/models/providers/index.ts
+++ src/lib/models/providers/index.ts
@@ -1,9 +1,11 @@
 import { ModelProviderUISection } from '@/lib/config/types';
 import { ProviderConstructor } from './baseProvider';
 import OpenAIProvider from './openai';
+import OllamaProvider from './ollama';
 
 export const providers: Record<string, ProviderConstructor<any>> = {
   openai: OpenAIProvider,
+  ollama: OllamaProvider
 };
 
 export const getModelProvidersUIConfigSection =
