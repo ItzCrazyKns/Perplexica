ff37225253b8a4bc9b09f2730ec14e5deb393d72
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-10-21 12:22:37 +0530

    feat(models-section): allow selecting chat model

--- src/components/Settings/Sections/Models/Section.tsx
+++ src/components/Settings/Sections/Models/Section.tsx
@@ -20,14 +20,25 @@ const Models = ({
   return (
     <div className="flex-1 space-y-6 overflow-y-auto py-6">
       <div className="flex flex-col px-6 gap-y-4">
-        <h3 className="text-sm text-black/70 dark:text-white/70">
+        <h3 className="text-xs lg:text-sm text-black/70 dark:text-white/70">
           Select models
         </h3>
-        <ModelSelect providers={values} type="embedding" />
+        <ModelSelect
+          providers={values.filter((p) =>
+            p.chatModels.some((m) => m.key != 'error'),
+          )}
+          type="chat"
+        />
+        <ModelSelect
+          providers={values.filter((p) =>
+            p.embeddingModels.some((m) => m.key != 'error'),
+          )}
+          type="embedding"
+        />
       </div>
       <div className="border-t border-light-200 dark:border-dark-200" />
       <div className="flex flex-row justify-between items-center px-6 ">
-        <p className="text-sm text-black/70 dark:text-white/70">
+        <p className="text-xs lg:text-sm text-black/70 dark:text-white/70">
           Manage model provider
         </p>
         <AddProvider modelProviders={fields} setProviders={setProviders} />
