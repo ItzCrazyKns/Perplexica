edba47aed86a7ad86e26299214a5e862f7dcd918
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-12-26 14:51:24 +0530

    feat(db): add migration scripts

--- drizzle/0002_daffy_wrecker.sql
+++ drizzle/0002_daffy_wrecker.sql
@@ -1,15 +1,1 @@
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
-CREATE TABLE `__new_messages` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`messageId` text NOT NULL,
-	`chatId` text NOT NULL,
-	`backendId` text NOT NULL,
-	`query` text NOT NULL,
-	`createdAt` text NOT NULL,
-	`responseBlocks` text DEFAULT '[]',
-	`status` text DEFAULT 'answering'
-);
---> statement-breakpoint
-DROP TABLE `messages`;--> statement-breakpoint
-ALTER TABLE `__new_messages` RENAME TO `messages`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;
+/* do nothing */
