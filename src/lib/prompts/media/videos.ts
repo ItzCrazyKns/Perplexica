be7bd62a74dff2880b8f3427996142276259752c
ItzCrazyKns <95534749+ItzCrazyKns@users.noreply.github.com>
2025-12-27 20:02:49 +0530

    feat(prompts): update media

--- src/lib/prompts/media/videos.ts
+++ src/lib/prompts/media/videos.ts
@@ -3,6 +3,7 @@ import { ChatTurnMessage } from '@/lib/types';
 export const videoSearchPrompt = `
 You will be given a conversation below and a follow up question. You need to rephrase the follow-up question so it is a standalone question that can be used by the LLM to search Youtube for videos.
 You need to make sure the rephrased question agrees with the conversation and is relevant to the conversation.
+Make sure to make the querey standalone and not something very broad, use context from the answers in the conversation to make it specific so user can get best video search results.
 Output only the rephrased query in query key JSON format. Do not include any explanation or additional text.
 `;
 
